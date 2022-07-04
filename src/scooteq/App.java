e2c0f543ea44dce601e153fe6ee9b099c2576976
vivien-laasch <vivien.laasch@outlook.com>
2022-07-04 13:18:17 +0200

    laaast comment

--- src/scooteq/App.java
+++ src/scooteq/App.java
@@ -8,6 +8,10 @@ import scooteq.view.MainView;
  * The Main class of the scooteq app. It creates a new {@code MainView}.
  */
 public class App {
+    
+    /** 
+     * @param args the arguments passed for the execution
+     */
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
